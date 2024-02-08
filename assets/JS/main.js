918e3e3fc7f99d0e23cf45ea4f24ee11f5ab290f
Patrick Bowen <patrickrbowen28@gmail.com>
2024-02-07 17:27:54 -0800

    minor styling to drink generation

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -152,12 +152,14 @@ function searchRecipies(cuisineType, dietType, includeIngredientsType) {
 }
 
 function getHistory() {
-  lastMealBtn.disabled = true
-  showIngredients()
-  showNewMealButton()
-  const lastMeal = JSON.parse(localStorage.getItem('pastResults'))
-  setAttr()
-  searchRecipies(lastMeal.cuisine, lastMeal.diet, lastMeal.includeIngredients)
+  lastMealBtn.disabled = true;
+  showIngredients();
+  showNewMealButton();
+  const lastMeal = JSON.parse(localStorage.getItem('pastResults'));
+  // Hide cuisineBox and show dynamicBox
+  document.getElementById('cuisineBox').style.display = 'none';
+  document.getElementById('dynamicBox').style.display = 'block';
+  searchRecipies(lastMeal.cuisine, lastMeal.diet, lastMeal.includeIngredients);
 }
 // showMoreMeals increasing the offset of the data until data runs out
 function showMoreMeals() {
@@ -214,56 +216,56 @@ function checkBoxInit() {
 
 // gets a fun random cocktail
 function createDrink() {
-  drinkContainerEl.innerHTML = ''
+  drinkContainerEl.innerHTML = '';
   let drinkUrl;
   if (localStorage.getItem('pastResultsCocktail') !== null) {
-    let lastDrink = localStorage.getItem('pastResultsCocktail')
-    lastDrink = lastDrink.slice(1, -1);
-    console.log(lastDrink)
-    drinkUrl = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${lastDrink}`
+      let lastDrink = localStorage.getItem('pastResultsCocktail');
+      lastDrink = lastDrink.slice(1, -1);
+      console.log(lastDrink);
+      drinkUrl = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${lastDrink}`;
   } else {
-    drinkUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+      drinkUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
   }
-  // const drinkUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
   fetch(drinkUrl)
-    .then((res) => res.json())
-    .then((data) => {
-      const drinkData = data.drinks[0];
-      const drinkId = drinkData.idDrink
-      const drinkTitle = drinkData.strDrink;
-      const imagesrc = drinkData.strDrinkThumb;
-      const drinkInstructions = drinkData.strInstructions;
-      const strIngredient = 'strIngredient'
-      let ingredientText = ''
-        for(let i = 1; i < maxItems; i++) {
-          let string = strIngredient + i
-          if (drinkData[string] !== null) {
-            ingredientText += (`<li class="has-text-primary">${drinkData[string]}</li>`)
+      .then((res) => res.json())
+      .then((data) => {
+          const drinkData = data.drinks[0];
+          const drinkTitle = drinkData.strDrink;
+          const imagesrc = drinkData.strDrinkThumb;
+          const drinkInstructions = drinkData.strInstructions;
+          const strIngredient = 'strIngredient';
+          let ingredientText = '';
+          for (let i = 1; i < maxItems; i++) {
+              let string = strIngredient + i;
+              if (drinkData[string] !== null) {
+                  ingredientText += `<li>${drinkData[string]}</li>`;
+              }
           }
-        }
-      const strMeasure = 'strMeasure'
-      let measureText = ''
-        for(let i = 1; i < maxItems; i++) {
-          let string = strMeasure + i
-          if (drinkData[string] !== null) {
-            measureText += (`<li class="has-text-primary">${drinkData[string]}</li>`)
+          const strMeasure = 'strMeasure';
+          let measureText = '';
+          for (let i = 1; i < maxItems; i++) {
+              let string = strMeasure + i;
+              if (drinkData[string] !== null) {
+                  measureText += `<li>${drinkData[string]}</li>`;
+              }
           }
-        }
-      const textContent = `
-        <h1>${drinkTitle}</h1>
-        <img src="${imagesrc}" alt="cocktail image" class= "image is-128x128 is-justify-content-center"></img>
-        <p class="has-text-primary">Instructions: ${drinkInstructions}</p>
-        <div class="column">
-        <li class="has-text-primary style="color: #77ccae;">Ingredients: ${ingredientText}</li>
-        </div>
-        <div class="column">
-        <li class="has-text-primary">Measurements: ${measureText}</li>
-        </div>
-    `;
-      const rowContent = `<section class="drinkData" class="row" >${textContent}</section>`;
-      drinkContainerEl.innerHTML += rowContent;
-      localStorage.setItem('pastResultsCocktail', JSON.stringify(drinkId))
-    });
+          const textContent = `
+              <div class="drink">
+                  <h1>${drinkTitle}</h1>
+                  <img src="${imagesrc}" alt="cocktail image">
+                  <p>Instructions: ${drinkInstructions}</p>
+                  <div class="ingredients">
+                      <h2>Ingredients:</h2>
+                      <ul>${ingredientText}</ul>
+                  </div>
+                  <div class="measures">
+                      <h2>Measurements:</h2>
+                      <ul>${measureText}</ul>
+                  </div>
+              </div>`;
+          drinkContainerEl.innerHTML += textContent;
+          localStorage.setItem('pastResultsCocktail', JSON.stringify(drinkData.idDrink));
+      });
 }
 // target selector for ingredient btns
 function checkIngredient(event) {
